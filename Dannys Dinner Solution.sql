a91b201c70f346e84722c62358f90cf64ba36ce7
Vicky Shrivastav <144537122+vickyku20@users.noreply.github.com>
2024-05-18 22:16:40 +0530

    Update Dannys Dinner Solution.sql

--- Dannys Dinner Solution.sql
+++ Dannys Dinner Solution.sql
@@ -161,7 +161,31 @@ FROM
     order by customer_id;
 
 
----11. Bonus Questions : Join All The Things
+-----BONUS QUESTION 1-------
+
+  Join All The Things
+ The following questions are related creating basic data tables that Danny and his team can use to quickly derive insights without needing to join the underlying tables using SQL.
+
+Recreate the following table output using the available data:
+
+customer_id	order_date	product_name	price	member
+A	2021-01-01	curry	15	N
+A	2021-01-01	sushi	10	N
+A	2021-01-07	curry	15	Y
+A	2021-01-10	ramen	12	Y
+A	2021-01-11	ramen	12	Y
+A	2021-01-11	ramen	12	Y
+B	2021-01-01	curry	15	N
+B	2021-01-02	curry	15	N
+B	2021-01-04	sushi	10	N
+B	2021-01-11	sushi	10	Y
+B	2021-01-16	ramen	12	Y
+B	2021-02-01	ramen	12	Y
+C	2021-01-01	ramen	12	N
+C	2021-01-01	ramen	12	N
+C	2021-01-07	ramen	12	N
+
+
 
 SELECT S.CUSTOMER_ID, ORDER_DATE, PRODUCT_NAME, PRICE,  
      CASE  
@@ -173,8 +197,43 @@ INNER JOIN MENU AS M ON S.PRODUCT_ID = M.PRODUCT_ID
 LEFT JOIN MEMBERS AS MEM ON MEM.CUSTOMER_ID = S.CUSTOMER_ID  
 ORDER BY 1,2,3,4 DESC;
 
+Output
 
----12. - RANK ALL THNGS
+customer_id  order_date  product_name  price  Members
+A	           2021-01-01	  sushi	        10	     N
+A	           2021-01-01	  curry	        15     	N
+A	           2021-01-07	  curry	        15	     Y
+A	           2021-01-10	  ramen	        12     	Y
+A	           2021-01-11	  ramen	        12	     Y
+A	           2021-01-11	  ramen	        12	     Y
+B	           2021-01-01	  curry	        15	     N
+B	           2021-01-02	  curry	        15	     N
+B	           2021-01-04	  sushi	        10	     N
+B	           2021-01-11	  sushi	        10	     Y
+B	           2021-01-16	  ramen	        12	     Y
+B	           2021-02-01	  ramen	        12	     Y
+	
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-----BONUS QUESTION 2-------
+
+Danny also requires further information about the ranking of customer products, but he purposely does not need the ranking for non-member purchases so he expects null ranking values for the records when customers are not yet part of the loyalty program.
+
+customer_id	order_date	product_name	price	member	ranking
+A	2021-01-01	curry	15	N	null
+A	2021-01-01	sushi	10	N	null
+A	2021-01-07	curry	15	Y	1
+A	2021-01-10	ramen	12	Y	2
+A	2021-01-11	ramen	12	Y	3
+A	2021-01-11	ramen	12	Y	3
+B	2021-01-01	curry	15	N	null
+B	2021-01-02	curry	15	N	null
+B	2021-01-04	sushi	10	N	null
+B	2021-01-11	sushi	10	Y	1
+B	2021-01-16	ramen	12	Y	2
+B	2021-02-01	ramen	12	Y	3
+C	2021-01-01	ramen	12	N	null
+C	2021-01-01	ramen	12	N	null
+C	2021-01-07	ramen	12	N	null
 
 WITH CTE AS (
 SELECT S.CUSTOMER_ID, ORDER_DATE, PRODUCT_NAME, PRICE,  
@@ -192,3 +251,21 @@ SELECT *,
             ELSE NULL
        END AS RNK 
    FROM CTE; 
+
+Output
+
+customer_id  order_date  join_date      product_name  price  Members   Ranking
+   A	       2021-01-01	   2021-01-07	    sushi	       10	       N	     null
+   A	       2021-01-01	   2021-01-07	    curry	       15	       N	     null
+   A	       2021-01-07	   2021-01-07	    curry	       15	       Y	      1
+   A	       2021-01-10	   2021-01-07	    ramen	       12	       Y	      2
+   A	       2021-01-11	   2021-01-07	    ramen	       12	       Y	      3
+   A	       2021-01-11	   2021-01-07	    ramen	       12	       Y	      3
+   B	       2021-01-01	   2021-01-09	    curry	       15	       N	     null
+   B	       2021-01-02	   2021-01-09	    curry	       15	       N	     null
+   B	       2021-01-04	   2021-01-09	    sushi	       10       	N	     null
+   B	       2021-01-11	   2021-01-09	    sushi	       10	       Y	      1
+   B	       2021-01-16	   2021-01-09	    ramen	       12	       Y	      2
+   B	       2021-02-01	   2021-01-09	   ramen	        12	       Y	      3
+
+
