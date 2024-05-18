91c6c164b2cffd8ea8131f41a77c9b11f19d7559
Vicky Shrivastav <144537122+vickyku20@users.noreply.github.com>
2024-05-18 22:01:32 +0530

    Update Dannys Dinner Solution.sql

--- Dannys Dinner Solution.sql
+++ Dannys Dinner Solution.sql
@@ -1,4 +1,22 @@
-## What is the total amount each customer spent at the restaurant?
+--Danny's Dinner - Taste of Success  SOLUTION
+--------------------------------------------------------------------------------------------
+
+-- 1. What is the total amount each customer spent at the restaurant?
+-- 2. How many days has each customer visited the restaurant?
+-- 3. What was the first item from the menu purchased by each customer?
+-- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+-- 5. Which item was the most popular for each customer?
+-- 6. Which item was purchased first by the customer after they became a member?
+-- 7. Which item was purchased just before the customer became a member?
+-- 8. What is the total items and amount spent for each member before they became a member?
+-- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+-- 10. In the first week after a customer joins the program (including their join date) 
+--     they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
+-- 11.Bonus Questions : Join All The Things
+-- 12. RANK ALL THNGS
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--1. What is the total amount each customer spent at the restaurant?
 
 select s.customer_id,
 sum(m.price) as tot_spent
@@ -6,16 +24,16 @@ from sales as s
 inner join menu m on m.product_id = s.product_id
 group by 1;
 
-## How many days has each customer visited the restaurant?
+
+--2. How many days has each customer visited the restaurant?
 
 select customer_id ,
 count(distinct order_date) as visited_restaurant
 from sales
 group by customer_id ;
 
-------------------------------------
 
-## What was the first item from the menu purchased by each customer?
+--3.  What was the first item from the menu purchased by each customer?
 
 SELECT customer_id, product_name
 FROM (
@@ -28,8 +46,7 @@ FROM (
 ) AS FirstPurchase
 WHERE rn = 1;
 
- ## 4. WHAT IS THE MOST PURCHASED ITEM ON THE MENU AND HOW MANY TIMES WAS IT 
-## PURCHASED BY ALL CUSTOMERS?
+--- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 
 select m.product_name,
 count(m.product_name) as no_of_times
@@ -39,7 +56,8 @@ group by m.product_name
 order by no_of_times desc
 limit 1;
 
-##5. WHICH ITEM WAS THE MOST POPULAR FOR EACH CUSTOMER?
+
+-- 5. Which item was the most popular for each customer?
 
 SELECT customer_id, product_name
 from (
@@ -51,8 +69,9 @@ inner join menu as m on m.product_id = s.product_id
 group by s.customer_id, m.product_name
 ) as purchased_cnt
 where rn = 1;
+
  
-##6. WHICH ITEM WAS PURCHASED FIRST BY THE CUSTOMER AFTER THEY BECAME A MEMBER
+-- 6. Which item was purchased first by the customer after they became a member?
 
 WITH orders AS (
     SELECT 
@@ -64,13 +83,14 @@ WITH orders AS (
     FROM menu AS m
     INNER JOIN sales AS s ON m.product_id = s.product_id
     JOIN members AS mb ON mb.customer_id = s.customer_id
-    WHERE s.order_date >mb.join_date
+    WHERE s.order_date > mb.join_date
 )
 SELECT customer_id, product_name
 FROM orders
 WHERE rn = 1;
 
-#### 7. WHICH ITEM WAS PURCHASED JUST BEFORE THE CUSTOMER BECAME A MEMBER?
+
+-- 7. Which item was purchased just before the customer became a member?
 
 WITH orders AS (
     SELECT 
@@ -89,50 +109,11 @@ FROM orders
 WHERE rn = 1;
  
  
- 
- -- Select the items purchased just before the customer became a member
-
-WITH CTE AS (
-    SELECT  
-        s.customer_id,  
-        s.order_date, 
-        mem.join_date, 
-        m.product_name,  
-        RANK() OVER(PARTITION BY s.customer_id ORDER BY s.order_date DESC) AS rnk
-    FROM  
-        sales AS s 
-    INNER JOIN 
-        menu AS m ON s.product_id = m.product_id 
-    INNER JOIN 
-        members AS mem ON mem.customer_id = s.customer_id  
-    WHERE  
-        s.order_date < mem.join_date
-)
-SELECT 
-    customer_id, 
-    product_name, 
-    order_date
-FROM 
-    CTE  
-WHERE  
-    rnk = 1
-ORDER BY
-    customer_id;
-
-)  
-SELECT CUSTOMER_ID, PRODUCT_NAME 
-FROM CTE  
-WHERE  
-RNK = 1; 
- 
- 
- 
- 
-#-#WHAT IS THE TOTAL ITEMS AND AMOUNT SPENT FOR EACH MEMBER BEFORE THEY BECAME A MEMBER?
+-- 8. What is the total items and amount spent for each member before they became a member?
 
 select s.customer_id,
 count(m.product_id) as tot_items,
-sum(m.price) as tot_spent
+Sum(m.price) as tot_spent
 FROM menu AS m
 INNER JOIN sales AS s ON m.product_id = s.product_id
 JOIN members AS mb ON mb.customer_id = s.customer_id
@@ -140,7 +121,7 @@ WHERE s.order_date < mb.join_date
 group by s.customer_id
 order by s.customer_id;
 
-##9. IF EACH $1 SPENT EQUATES TO 10 POINTS AND SUSHI HAS A 2X POINTS MULTIPLIER HOW MANY POINTS WOULD EACH CUSTOMER HAVE?
+--9. IF EACH $1 SPENT EQUATES TO 10 POINTS AND SUSHI HAS A 2X POINTS MULTIPLIER HOW MANY POINTS WOULD EACH CUSTOMER HAVE?
 
 SELECT s.customer_id,
     SUM(
@@ -157,9 +138,7 @@ GROUP BY
     s.customer_id;
 
 
-## ---10. IN THE FIRST WEEK AFTER A CUSTOMER JOINS THE PROGRAM (INCLUDING THEIR JOIN DATE) -----
-  #---THEY EARN 2X POINTS ON ALL ITEMS AFTER THAT 1X, NOT JUST SUSHI  ----
-  # ---HOW MANY POINTS DO CUSTOMER A AND B HAVE AT THE END OF JANUARY?
+-- 10. In the first week after a customer joins the program (including their join date)  they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
 
 WITH purchase_points AS (
     SELECT 
@@ -181,7 +160,8 @@ FROM
     group by customer_id
     order by customer_id;
 
-#11. Bonus Questions : Join All The Things
+
+---11. Bonus Questions : Join All The Things
 
 SELECT S.CUSTOMER_ID, ORDER_DATE, PRODUCT_NAME, PRICE,  
      CASE  
@@ -193,7 +173,8 @@ INNER JOIN MENU AS M ON S.PRODUCT_ID = M.PRODUCT_ID
 LEFT JOIN MEMBERS AS MEM ON MEM.CUSTOMER_ID = S.CUSTOMER_ID  
 ORDER BY 1,2,3,4 DESC;
 
-# - RANK ALL THNGS
+
+---12. - RANK ALL THNGS
 
 WITH CTE AS (
 SELECT S.CUSTOMER_ID, ORDER_DATE, PRODUCT_NAME, PRICE,  
